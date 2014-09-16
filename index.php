5df7bd111ca3010770c9ded2b7bc2e2ab46d6cff
Kristi Henno <hennokri@gmail.com>
2014-09-16 17:58:00 +0300

    Add sample variables
    Add sample variables

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>PHP põhitõed II</title>
+</head>
+
+<body>
+	<h1>Muutujad</h1>
+	<?php
+    $hernes = 20;
+    echo $hernes;
+	echo "<br>";
+	
+
+    $hernes = 30;
+    echo $hernes;
+	echo "<br>";
+?>
+
+	
+</body>
+</html>	
